9d15a865543317c9f2c61ea390ce10d75475eea2
Kole565 <rjkz565rjkz565@gmail.com>
2021-12-22 16:15:45 +0300

    RecordIO base create

--- lib/record_io.py
+++ lib/record_io.py
@@ -1,5 +1,35 @@
 class RecordIO():
     
-    def __init__(self):
-        pass
+    """RecordIO class
     
+    Ask user for training data. Untestable because IO.
+
+    """
+
+    def __init__(self):
+        self.input_buffer = {}
+    
+    def ask_save_multi(self, names):
+        for name in names:
+            self.ask(name)
+            self.get_and_save(name)
+
+    def ask(self, name):
+        print("Enter {0}:".format(name.capitalize()))
+    
+    def get_and_save(self, name):
+        self.input_buffer[name.lower()] = input()
+    
+    def show_buffer(self):
+        for name, value in self.input_buffer.items():
+            print("{0}: {1}".format(name.capitalize(), value))
+    
+if __name__ == "__main__":
+    recorder = RecordIO()
+
+    recorder.ask("test")
+    recorder.get_and_save("test")
+
+    recorder.ask_save_multi(["time", "date", "duration"])
+
+    recorder.show_buffer()
