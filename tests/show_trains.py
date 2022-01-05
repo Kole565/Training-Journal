e74a563f0570494bd5cf92708443b5b8c23b1917
Kole565 <rjkz565rjkz565@gmail.com>
2022-01-05 10:55:17 +0300

    Update .gitignore. Add TODO-s.

---
+++ tests/show_trains.py
@@ -0,0 +1,6 @@
+# TODO:
+#   Create show_all test
+#   Create show_by test
+#   
+#   Create show_period test    (need to refine data types before)
+#   Create show_sorted_by test (need to refine data types before)
